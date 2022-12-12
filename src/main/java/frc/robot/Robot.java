f82657a4d4aaed1795bfbcf2ca8ffca0fe22b541
frc3883 <outreach3883@gmail.com>
2022-12-12 17:36:03 -0600

    initial commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,14 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.Vector2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import com.ctre.phoenix.sensors.Pigeon2;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
@@ -18,7 +22,9 @@ public class Robot extends TimedRobot {
   private final VictorSP m_rightMotor = new VictorSP(0);
   private final VictorSP m_launchMotor = new VictorSP(3);
   private final VictorSP m_stageMotor = new VictorSP(2);
-  
+  private final PIDController m_rotController = new PIDController(1, 0, 0);
+  private final PIDController m_driveController = new PIDController(1, 0, 0);
+  private final Pigeon2 gyro = new Pigeon2(4);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   private final Joystick m_stick = new Joystick(0);
   private final Joystick m_overrideStick = new Joystick(1);
@@ -28,6 +34,7 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
+    
     m_leftMotor.setInverted(true);
     
   }
@@ -37,8 +44,16 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    
+    Vector2d input = new Vector2d(m_stick.getX(), m_stick.getY());
+    input.rotate(gyro.getYaw());
 
+    if (m_rotController.atSetpoint()){
+      m_robotDrive.arcadeDrive(m_driveController.calculate(input.magnitude()), 0f);
+    }
+    else{
+      m_robotDrive.arcadeDrive(0f, m_rotController.calculate(Math.toDegrees(Math.atan2(input.y, input.x))));
+    }
+    /*
     if (m_stick.getTrigger()){
       m_launchMotor.set(0.25);
     }
@@ -58,6 +73,8 @@ public class Robot extends TimedRobot {
     }
     else{
       m_robotDrive.arcadeDrive(-m_stick.getY()*(1f-m_overrideStick.getThrottle() + 0.25f), -m_stick.getX());
-    }
+    }*/
+
+
   }
 }
